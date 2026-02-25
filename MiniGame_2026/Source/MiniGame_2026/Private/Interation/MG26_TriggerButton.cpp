7aaa8a19587c0ce0ba3c168207f5d31fe7f39b98
anhpt135 <84607090+anhpt135@users.noreply.github.com>
2026-02-25 10:50:11 +0700

    03_Cấu hình ProjectSetting

---
+++ MiniGame_2026/Source/MiniGame_2026/Private/Interation/MG26_TriggerButton.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interation/MG26_TriggerButton.h"
+
+// Sets default values
+AMG26_TriggerButton::AMG26_TriggerButton()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMG26_TriggerButton::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMG26_TriggerButton::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
