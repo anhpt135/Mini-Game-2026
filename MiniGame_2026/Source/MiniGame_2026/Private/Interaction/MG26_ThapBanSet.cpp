be5fdbd3a0ebda19616aefdcec10e772362ef749
anhpt135 <84607090+anhpt135@users.noreply.github.com>
2026-03-05 15:27:42 +0700

    19_Tạo class Actor làm Tháp Bắn Sét

---
+++ MiniGame_2026/Source/MiniGame_2026/Private/Interaction/MG26_ThapBanSet.cpp
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interaction/MG26_ThapBanSet.h"
+
+
+// Sets default values
+AMG26_ThapBanSet::AMG26_ThapBanSet()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AMG26_ThapBanSet::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMG26_ThapBanSet::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ MiniGame_2026/Source/MiniGame_2026/Public/Interaction/MG26_ThapBanSet.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MG26_ThapBanSet.generated.h"
+
+UCLASS()
+class MINIGAME_2026_API AMG26_ThapBanSet : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AMG26_ThapBanSet();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
