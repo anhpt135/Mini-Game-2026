2e6fe3ad04845f6e014df12d632fc75f23d2611a
anhpt135 <84607090+anhpt135@users.noreply.github.com>
2026-03-05 16:24:20 +0700

    20_Tạo logic bắn nhân vật khi đi vào Tháp

--- MiniGame_2026/Source/MiniGame_2026/Private/Interaction/MG26_ThapBanSet.cpp
+++ MiniGame_2026/Source/MiniGame_2026/Private/Interaction/MG26_ThapBanSet.cpp
@@ -1,26 +1,115 @@
 ﻿// Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "Interaction/MG26_ThapBanSet.h"
-
+#include "Components/SphereComponent.h"
+#include "NiagaraComponent.h"
+#include "NiagaraFunctionLibrary.h"
+#include "Kismet/GameplayStatics.h"
+#include "GameFramework/Pawn.h"
 
 // Sets default values
 AMG26_ThapBanSet::AMG26_ThapBanSet()
 {
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	// Tạo Sphere Component và set làm root
+	DetectionSphere = CreateDefaultSubobject<USphereComponent>(TEXT("DetectionSphere"));
+	RootComponent = DetectionSphere;
+
+	// Thiết lập các thuộc tính cho sphere
+	DetectionSphere->SetSphereRadius(1000.f); // Tầm bắn mặc định 1000cm (10m)
+	DetectionSphere->SetCollisionProfileName(TEXT("OverlapAllDynamic"));
+
+	// Tạo Niagara Component
+	LightningComponent = CreateDefaultSubobject<UNiagaraComponent>(TEXT("LightningComponent"));
+	LightningComponent->SetupAttachment(RootComponent);
+	LightningComponent->bAutoActivate = false; // Mặc định tắt, chỉ bật khi có mục tiêu
 }
 
 // Called when the game starts or when spawned
 void AMG26_ThapBanSet::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	// Đảm bảo tắt hiệu ứng khi bắt đầu game
+	if (LightningComponent)
+	{
+		LightningComponent->Deactivate();
+	}
+
+	// Bind sự kiện overlap
+	DetectionSphere->OnComponentBeginOverlap.AddDynamic(this, &AMG26_ThapBanSet::OnOverlapBegin);
+	DetectionSphere->OnComponentEndOverlap.AddDynamic(this, &AMG26_ThapBanSet::OnOverlapEnd);
+}
+
+void AMG26_ThapBanSet::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+
+	// Logic chỉ chạy trong Editor để Preview
+#if WITH_EDITOR
+	if (LightningComponent && LightningComponent->GetAsset())
+	{
+		// Nếu không phải đang chạy game (PIE/Standalone)
+		if (!GetWorld() || !GetWorld()->IsGameWorld())
+		{
+			// Bật hiệu ứng để xem trước
+			LightningComponent->Activate(true);
+
+			// Đặt một vị trí mục tiêu giả định (cách tháp 500 đơn vị về phía trước)
+			FVector PreviewTargetLocation = GetActorLocation() + GetActorForwardVector() * 500.f;
+			LightningComponent->SetVectorParameter(LightningTargetParamName, PreviewTargetLocation);
+		}
+	}
+#endif
 }
 
 // Called every frame
 void AMG26_ThapBanSet::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+	// Nếu có mục tiêu và hiệu ứng đang chạy, cập nhật vị trí mục tiêu cho Niagara
+	if (TargetActor && LightningComponent->IsActive())
+	{
+		FVector TargetLocation = TargetActor->GetActorLocation();
+		LightningComponent->SetVectorParameter(LightningTargetParamName, TargetLocation);
+	}
 }
 
+void AMG26_ThapBanSet::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	// Chỉ tấn công Pawn (Character hoặc Pawn thường) và khi chưa có mục tiêu
+	if (OtherActor && OtherActor->IsA(APawn::StaticClass()) && !TargetActor)
+	{
+		// Kiểm tra xem actor có phải là pawn do người chơi điều khiển không
+		APawn* PlayerPawn = Cast<APawn>(OtherActor);
+		if (PlayerPawn && PlayerPawn->IsPlayerControlled())
+		{
+			TargetActor = PlayerPawn;
+
+			// Bật hiệu ứng Niagara
+			if (LightningComponent)
+			{
+				LightningComponent->Activate(true);
+			}
+		}
+	}
+}
+
+void AMG26_ThapBanSet::OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	// Nếu actor rời đi chính là mục tiêu hiện tại
+	if (OtherActor && OtherActor == TargetActor)
+	{
+		// Tắt hiệu ứng Niagara
+		if (LightningComponent)
+		{
+			LightningComponent->Deactivate();
+		}
+
+		// Xóa tham chiếu đến mục tiêu
+		TargetActor = nullptr;
+	}
+}
--- MiniGame_2026/Source/MiniGame_2026/Public/Interaction/MG26_ThapBanSet.h
+++ MiniGame_2026/Source/MiniGame_2026/Public/Interaction/MG26_ThapBanSet.h
@@ -6,6 +6,9 @@
 #include "GameFramework/Actor.h"
 #include "MG26_ThapBanSet.generated.h"
 
+class USphereComponent;
+class UNiagaraComponent;
+
 UCLASS()
 class MINIGAME_2026_API AMG26_ThapBanSet : public AActor
 {
@@ -19,7 +22,35 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	// Được gọi khi Actor được tạo hoặc thay đổi trong Editor
+	virtual void OnConstruction(const FTransform& Transform) override;
+
 public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+
+protected:
+	/** Vùng phát hiện mục tiêu */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	USphereComponent* DetectionSphere;
+
+	/** Component Niagara để hiển thị tia sét. Gán Asset vào đây trong Blueprint. */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	UNiagaraComponent* LightningComponent;
+
+	/** Tên tham số vector trong Niagara để set vị trí mục tiêu (User Parameter) */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Effects")
+	FName LightningTargetParamName = FName("User.TargetLocation");
+
+	/** Mục tiêu hiện tại đang bị tấn công */
+	UPROPERTY()
+	AActor* TargetActor;
+
+	/** Hàm xử lý khi có Actor đi vào vùng ảnh hưởng */
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	/** Hàm xử lý khi có Actor rời khỏi vùng ảnh hưởng */
+	UFUNCTION()
+	void OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 };
