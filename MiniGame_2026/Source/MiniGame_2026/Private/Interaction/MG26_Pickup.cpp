b9e2c1bc1691a3a6ee46e2286b043aa92017573f
anhpt135 <84607090+anhpt135@users.noreply.github.com>
2026-03-20 09:19:27 +0700

    36_Sửa linh tinh

--- MiniGame_2026/Source/MiniGame_2026/Private/Interaction/MG26_Pickup.cpp
+++ MiniGame_2026/Source/MiniGame_2026/Private/Interaction/MG26_Pickup.cpp
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Điền thông báo bản quyền của bạn trong trang Mô tả của Project Settings.
 
 
 #include "Interaction/MG26_Pickup.h"
@@ -6,60 +6,65 @@
 #include "Components/StaticMeshComponent.h"
 #include "Components/LocalLightComponent.h"
 
-// Sets default values
+// Thiết lập các giá trị mặc định
 AMG26_Pickup::AMG26_Pickup()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+ 	// Thiết lập Actor này gọi hàm Tick() mỗi frame. Bạn có thể tắt nếu không cần để tối ưu hiệu suất.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// Tạo Component Sphere để kiểm tra va chạm
 	OverlapSphere = CreateDefaultSubobject<USphereComponent>(TEXT("OverlapSphere"));
 	RootComponent = OverlapSphere;
-	// Ignore Camera Collision
-	OverlapSphere->SetCollisionResponseToChannel(ECC_Camera, ECR_Ignore);
 
+	// Tạo Component Static Mesh để hiển thị hình ảnh vật phẩm
 	PickupMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("PickupMesh"));
 	PickupMesh->SetupAttachment(RootComponent);
-	// Ignore Camera Collision
-	PickupMesh->SetCollisionResponseToChannel(ECC_Camera, ECR_Ignore);
 
 	// Chúng ta không tạo cứng PointLight hay SpotLight trong C++ nữa.
 	// Bạn sẽ chủ động thêm Component (PointLight hoặc SpotLight) trực tiếp vào Blueprint
 	// C++ sẽ tự động tìm thấy cái đèn đó lúc BeginPlay()
 	PickupLight = nullptr;
 
+	// Đặt giá trị độ sáng tối đa mặc định
 	MaxLightIntensity = 5000.f;
 }
 
-// Called when the game starts or when spawned
 void AMG26_Pickup::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	// Lưu lại kích thước ban đầu của Mesh để sử dụng cho animation
 	InitialScale = PickupMesh->GetRelativeScale3D();
 
-	// Tự động tìm Component đèn (LocalLightComponent - Cha của PointLight & SpotLight)
+	// Tự động tìm Component đèn (LocalLightComponent - Cha của PointLight & SpotLight) có trong Actor
 	PickupLight = Cast<ULocalLightComponent>(GetComponentByClass(ULocalLightComponent::StaticClass()));
 
+	// Nếu tìm thấy đèn, tắt độ sáng lúc mới bắt đầu
 	if (PickupLight)
 	{
 		PickupLight->SetIntensity(0.f);
 	}
 
+	// Thiết lập Timeline nếu các Curve (đường cong) đã được gán
 	if (ScaleCurve && LightCurve)
 	{
 		FOnTimelineEvent TimelineUpdateDelegate;
 		TimelineUpdateDelegate.BindUFunction(this, FName("TimelineUpdate"));
 
-		PickupTimeline.AddInterpFloat(ScaleCurve, FOnTimelineFloat()); // Dummy float interp just to drive length
+		// Thêm một giá trị float giả để Timeline có thể chạy và lấy độ dài thời gian từ ScaleCurve
+		PickupTimeline.AddInterpFloat(ScaleCurve, FOnTimelineFloat()); 
+		// Gán hàm cập nhật (TimelineUpdate) sẽ chạy mỗi khi Timeline thay đổi
 		PickupTimeline.SetTimelinePostUpdateFunc(TimelineUpdateDelegate);
 	}
 
+	// Đăng ký các sự kiện Overlap (bắt đầu và kết thúc) với các hàm tương ứng
 	OverlapSphere->OnComponentBeginOverlap.AddDynamic(this, &AMG26_Pickup::OnOverlapBegin);
 	OverlapSphere->OnComponentEndOverlap.AddDynamic(this, &AMG26_Pickup::OnOverlapEnd);
 }
 
 void AMG26_Pickup::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
+	// Nếu Actor chạm vào là một Pawn (ví dụ: Nhân vật người chơi), thì phát Timeline tiến tới
 	if (OtherActor && OtherActor != this && OtherActor->IsA(APawn::StaticClass()))
 	{
 		PickupTimeline.Play();
@@ -68,6 +73,7 @@ void AMG26_Pickup::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AAct
 
 void AMG26_Pickup::OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
 {
+	// Nếu Pawn đi ra khỏi vùng va chạm, thì phát Timeline lùi lại (để đảo ngược hiệu ứng)
 	if (OtherActor && OtherActor != this && OtherActor->IsA(APawn::StaticClass()))
 	{
 		PickupTimeline.Reverse();
@@ -76,14 +82,17 @@ void AMG26_Pickup::OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor
 
 void AMG26_Pickup::TimelineUpdate()
 {
+	// Lấy thời gian hiện tại của Timeline
 	float CurrentPlaybackTime = PickupTimeline.GetPlaybackPosition();
 
+	// Cập nhật kích thước (Scale) của Mesh dựa trên giá trị của ScaleCurve tại thời điểm hiện tại
 	if (ScaleCurve)
 	{
 		float ScaleFactor = ScaleCurve->GetFloatValue(CurrentPlaybackTime);
 		PickupMesh->SetRelativeScale3D(InitialScale * ScaleFactor);
 	}
 
+	// Cập nhật độ sáng của đèn dựa trên giá trị của LightCurve tại thời điểm hiện tại
 	if (LightCurve && PickupLight)
 	{
 		float LightIntensity = LightCurve->GetFloatValue(CurrentPlaybackTime);
@@ -91,11 +100,11 @@ void AMG26_Pickup::TimelineUpdate()
 	}
 }
 
-// Called every frame
 void AMG26_Pickup::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	// Nếu Timeline đang phát (tiến hoặc lùi), tiến hành cập nhật Timeline theo thời gian
 	if (PickupTimeline.IsPlaying() || PickupTimeline.IsReversing())
 	{
 		PickupTimeline.TickTimeline(DeltaTime);
--- MiniGame_2026/Source/MiniGame_2026/Public/Interaction/MG26_Pickup.h
+++ MiniGame_2026/Source/MiniGame_2026/Public/Interaction/MG26_Pickup.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Điền thông báo bản quyền của bạn trong trang Mô tả của Project Settings.
 
 #pragma once
 
@@ -18,16 +18,18 @@ class MINIGAME_2026_API AMG26_Pickup : public AActor
 	GENERATED_BODY()
 	
 public:	
-	// Sets default values for this actor's properties
+	// Thiết lập các giá trị mặc định cho các thuộc tính của Actor này
 	AMG26_Pickup();
 
 protected:
-	// Called when the game starts or when spawned
+	// Được gọi khi trò chơi bắt đầu hoặc khi Actor được sinh ra (spawned)
 	virtual void BeginPlay() override;
 
+	// Component hình cầu dùng để kiểm tra va chạm (Overlap)
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
 	USphereComponent* OverlapSphere;
 
+	// Component hiển thị hình ảnh 3D (Mesh) của vật phẩm
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
 	UStaticMeshComponent* PickupMesh;
 
@@ -35,31 +37,38 @@ protected:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
 	ULocalLightComponent* PickupLight;
 
-	// Cấu trúc Timeline
+	// Cấu trúc Timeline dùng để tạo hiệu ứng hoạt ảnh (animation) theo thời gian
 	FTimeline PickupTimeline;
 
+	// Đường cong (Curve) định nghĩa sự thay đổi kích thước (Scale) của vật phẩm
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Animation")
 	UCurveFloat* ScaleCurve;
 
+	// Đường cong (Curve) định nghĩa sự thay đổi độ sáng (Intensity) của đèn
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Animation")
 	UCurveFloat* LightCurve;
 
+	// Độ sáng tối đa của đèn khi được kích hoạt
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Animation")
 	float MaxLightIntensity;
 
+	// Lưu trữ kích thước ban đầu của vật phẩm
 	FVector InitialScale;
 
+	// Khai báo hàm Callback khi có một Actor khác bắt đầu đi vào vùng Sphere (Overlap)
 	UFUNCTION()
 	void OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
+	// Khai báo hàm Callback khi một Actor khác đi ra khỏi vùng Sphere
 	UFUNCTION()
 	void OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 
+	// Hàm được gọi mỗi khi Timeline cập nhật (chạy theo từng frame)
 	UFUNCTION()
 	void TimelineUpdate();
 
 public:	
-	// Called every frame
+	// Được gọi mỗi frame (khung hình)
 	virtual void Tick(float DeltaTime) override;
 
 };
