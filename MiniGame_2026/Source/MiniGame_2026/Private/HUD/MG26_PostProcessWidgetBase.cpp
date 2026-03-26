6468a718cd376ff03e3abfb49cff24c1dccb0f95
anhpt135 <84607090+anhpt135@users.noreply.github.com>
2026-03-26 09:53:47 +0700

    45_Thiết lập trong C++(Tạo class C++ Widget)

---
+++ MiniGame_2026/Source/MiniGame_2026/Private/HUD/MG26_PostProcessWidgetBase.cpp
@@ -0,0 +1,3 @@
+
+#include "HUD/MG26_PostProcessWidgetBase.h"
+
---
+++ MiniGame_2026/Source/MiniGame_2026/Public/HUD/MG26_PostProcessWidgetBase.h
@@ -0,0 +1,13 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MG26_PostProcessWidgetBase.generated.h"
+
+UCLASS()
+class MINIGAME_2026_API UMG26_PostProcessWidgetBase : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
